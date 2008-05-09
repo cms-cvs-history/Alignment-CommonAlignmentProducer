c896ad16feb1945900d09bb9d05063defd5d35c8
cmsbuild <>
2008-05-09 11:49:08 +0000

    This commit was manufactured by cvs2git to create tag 'Alignment- CommonAlignmentProducer-V00-27-14-07'.
    Sprout from master 2008-05-09 11:49:07 UTC David Futyan <sha1-9b33e1ceb82d36509264da3be42fb8b223a33816@cern.ch> 'add MEtoEDMConverter to event content'
    Cherrypick from master 2008-03-14 22:30:39 UTC Jim Pivarski <sha1-4b35a9abbbfdbbd8cd887eda503efe9ec3a292e0@cern.ch> 'corrected AlCa paths after (slightly botched) cloning of modules':
        data/ALCARECOMuAlZMuMu.cff
        data/AlignmentProducer.cff
        data/AlignmentProducerCSA07.cff
        interface/AlignmentTrackSelector.h
        plugins/AlignmentProducer.cc
        plugins/AlignmentTrackSelectorModule.cc
        src/AlignmentCSCOverlapSelector.cc
        src/AlignmentTrackSelector.cc
        test/authentication.xml
        test/make_muonalignment_dirs.pl
    Delete:
        data/ALCARECOMuAlCalIsolatedMu.cff
        data/ALCARECOMuAlCalIsolatedMu_Output.cff
        data/ALCARECOMuCaliMinBias.cff
        data/ALCARECOMuCaliMinBias_Output.cff
        data/ALCARECOTkAlCosmics0T.cff
        data/ALCARECOTkAlCosmicsCTF0T.cfi
        data/ALCARECOTkAlCosmicsCosmicTF0T.cfi
        python/ALCARECOMuAlBeamHaloOverlaps_Output_cff.py
        python/ALCARECOMuAlBeamHaloOverlaps_cff.py
        python/ALCARECOMuAlBeamHalo_Output_cff.py
        python/ALCARECOMuAlBeamHalo_cff.py
        python/ALCARECOMuAlOverlaps_Output_cff.py
        python/ALCARECOMuAlOverlaps_cff.py
        python/ALCARECOMuAlZMuMu_Output_cff.py
        python/ALCARECOMuAlZMuMu_cff.py
        python/ALCARECOTkAlBeamHalo_Output_cff.py
        python/ALCARECOTkAlBeamHalo_cff.py
        python/ALCARECOTkAlCosmicsCTF_cfi.py
        python/ALCARECOTkAlCosmicsCosmicTF_cfi.py
        python/ALCARECOTkAlCosmics_Output_cff.py
        python/ALCARECOTkAlCosmics_cff.py
        python/ALCARECOTkAlJpsiMuMu_Output_cff.py
        python/ALCARECOTkAlJpsiMuMu_cff.py
        python/ALCARECOTkAlMinBias_Output_cff.py
        python/ALCARECOTkAlMinBias_cff.py
        python/ALCARECOTkAlMuonIsolated_Output_cff.py
        python/ALCARECOTkAlMuonIsolated_cff.py
        python/ALCARECOTkAlUpsilonMuMu_Output_cff.py
        python/ALCARECOTkAlUpsilonMuMu_cff.py
        python/ALCARECOTkAlWMuNu_Output_cff.py
        python/ALCARECOTkAlWMuNu_cff.py
        python/ALCARECOTkAlZMuMu_Output_cff.py
        python/ALCARECOTkAlZMuMu_cff.py
        python/AlignmentMuonSelector_cfi.py
        python/AlignmentProducerCSA07_cff.py
        python/AlignmentProducer_cff.py
        python/AlignmentTrackSelector_cfi.py
        python/DBConfiguration_cff.py
        python/GlobalPosition_Fake_cff.py
        python/GlobalPosition_Frontier_DevDB_cff.py
        python/GlobalPosition_Frontier_IntDB_cff.py
        python/GlobalPosition_Frontier_cff.py
        python/fakeAlignmentProducer_cfi.py

--- interface/AlignmentTrackSelector.h
+++ interface/AlignmentTrackSelector.h
@@ -27,9 +27,6 @@ class AlignmentTrackSelector
 
   /// select tracks
   Tracks select(const Tracks& tracks, const edm::Event& evt) const;
-  ///returns if any of the Filters is used.
-  bool useThisFilter();
-
 
  private:
 
