9fab1b89c952929e6ce416c78e41bd4c89c9bf3e
cmsbuild <>
2008-05-15 23:57:33 +0000

    This commit was manufactured by cvs2git to create tag 'Alignment- CommonAlignmentProducer-CMSSW_2_0_11'.
    Sprout from master 2008-05-15 23:57:31 UTC Benedikt Hegner <sha1-0de55b49d81c4ae1a8d1abe947debaac37f62035@cern.ch> 'now self contained and working in 20X'
    Cherrypick from master 2008-03-27 18:31:11 UTC Gero Flucke <sha1-088957c4b229a756df932b4fa3fae85c4cd10a7f@cern.ch> 'make 200 aware':
        data/ALCARECOMuAlZMuMu.cff
        data/AlignmentProducer.cff
        data/AlignmentProducerCSA07.cff
        interface/AlignmentTrackSelector.h
        plugins/AlignmentTrackSelectorModule.cc
        src/AlignmentCSCOverlapSelector.cc
        src/AlignmentTrackSelector.cc
        test/alignment.cfg
        test/authentication.xml
        test/make_muonalignment_dirs.pl
    Delete:
        data/ALCARECOMuAlCalIsolatedMu.cff
        data/ALCARECOMuAlCalIsolatedMu_Output.cff
        data/ALCARECOMuCaliMinBias.cff
        data/ALCARECOMuCaliMinBias_Output.cff
        python/ALCARECOMuAlBeamHaloOverlaps_Output_cff.py
        python/ALCARECOMuAlBeamHaloOverlaps_cff.py
        python/ALCARECOMuAlBeamHalo_Output_cff.py
        python/ALCARECOMuAlBeamHalo_cff.py
        python/ALCARECOMuAlOverlaps_Output_cff.py
        python/ALCARECOMuAlOverlaps_cff.py
        python/ALCARECOMuAlZMuMu_Output_cff.py
        python/ALCARECOMuAlZMuMu_cff.py
        python/ALCARECOMuCaliMinBias_Output_cff.py
        python/ALCARECOMuCaliMinBias_cff.py
        python/ALCARECOTkAlBeamHalo_Output_cff.py
        python/ALCARECOTkAlBeamHalo_cff.py
        python/ALCARECOTkAlCosmics0THLT_Output_cff.py
        python/ALCARECOTkAlCosmics0THLT_cff.py
        python/ALCARECOTkAlCosmics0T_Output_cff.py
        python/ALCARECOTkAlCosmics0T_cff.py
        python/ALCARECOTkAlCosmicsCTF_cfi.py
        python/ALCARECOTkAlCosmicsCosmicTF_cfi.py
        python/ALCARECOTkAlCosmicsHLT_Output_cff.py
        python/ALCARECOTkAlCosmicsHLT_cff.py
        python/ALCARECOTkAlCosmics_Output_cff.py
        python/ALCARECOTkAlCosmics_cff.py
        python/ALCARECOTkAlJpsiMuMu_Output_cff.py
        python/ALCARECOTkAlJpsiMuMu_cff.py
        python/ALCARECOTkAlLAS_Output_cff.py
        python/ALCARECOTkAlLAS_cff.py
        python/ALCARECOTkAlMinBias_Output_cff.py
        python/ALCARECOTkAlMinBias_cff.py
        python/ALCARECOTkAlMuonIsolated_Output_cff.py
        python/ALCARECOTkAlMuonIsolated_cff.py
        python/ALCARECOTkAlUpsilonMuMu_Output_cff.py
        python/ALCARECOTkAlUpsilonMuMu_cff.py
        python/ALCARECOTkAlWMuNu_Output_cff.py
        python/ALCARECOTkAlWMuNu_cff.py
        python/ALCARECOTkAlZMuMu_Output_cff.py
        python/ALCARECOTkAlZMuMu_cff.py
        python/AlignmentMuonSelector_cfi.py
        python/AlignmentProducerCSA07_cff.py
        python/AlignmentProducer_cff.py
        python/AlignmentTrackSelector_cfi.py
        python/DBConfiguration_cff.py
        python/GlobalPosition_Fake_cff.py
        python/GlobalPosition_Frontier_DevDB_cff.py
        python/GlobalPosition_Frontier_IntDB_cff.py
        python/GlobalPosition_Frontier_cff.py
        python/fakeAlignmentProducer_cfi.py

--- src/AlignmentTrackSelector.cc
+++ src/AlignmentTrackSelector.cc
@@ -9,11 +9,9 @@
 #include "DataFormats/TrackerRecHit2D/interface/SiStripRecHit2D.h"
 #include "DataFormats/TrackerRecHit2D/interface/ProjectedSiStripRecHit2D.h"
 #include "DataFormats/TrackerRecHit2D/interface/SiStripMatchedRecHit2D.h"
-#include "DataFormats/TrackerRecHit2D/interface/SiTrackerMultiRecHit.h"
 #include "DataFormats/SiStripCluster/interface/SiStripCluster.h"
 #include "DataFormats/TrackerRecHit2D/interface/SiStripMatchedRecHit2DCollection.h"
 #include "DataFormats/TrackerRecHit2D/interface/SiStripRecHit2DCollection.h"
-
 
 #include "DataFormats/DetId/interface/DetId.h"
 #include "DataFormats/SiStripDetId/interface/StripSubdetector.h"
@@ -57,32 +55,17 @@ AlignmentTrackSelector::AlignmentTrackSelector(const edm::ParameterSet & cfg) :
   minHitsinBPIX_ (cfg.getParameter<edm::ParameterSet>( "minHitsPerSubDet" ).getParameter<int>( "inBPIX" ) ),
   minHitsinFPIX_ (cfg.getParameter<edm::ParameterSet>( "minHitsPerSubDet" ).getParameter<int>( "inFPIX" ) )
 {
-  if (applyBasicCuts_){
-      edm::LogInfo("AlignmentTrackSelector") 
-	<< "applying basic track cuts ..."
-	<< "\nptmin,ptmax:     " << ptMin_   << "," << ptMax_ 
-	<< "\netamin,etamax:   " << etaMin_  << "," << etaMax_
-	<< "\nphimin,phimax:   " << phiMin_  << "," << phiMax_
-	<< "\nnhitmin,nhitmax: " << nHitMin_ << "," << nHitMax_
-	<< "\nnhitmin2D:       " << nHitMin2D_
-	<< "\nchi2nmax:        " << chi2nMax_;
 
-      if (applyIsolation_)
+  if (applyBasicCuts_)
 	edm::LogInfo("AlignmentTrackSelector") 
-	  << "only retain tracks isolated at least by " << minHitIsolation_ << 
-	  " cm from other rechits"; 
-      
-      if (chargeCheck_)
-	edm::LogInfo("AlignmentTrackSelector") 
-	  << "only retain hits with at least " << minHitChargeStrip_ <<  
-	  " ADC counts of total cluster charge"; 
-      
-      edm::LogInfo("AlignmentTrackSelector") 
-	<< "Minimum number of hits in TIB/TID/TOB/TEC/BPIX/FPIX = " 
-	<< minHitsinTIB_ << "/" << minHitsinTID_ << "/" << minHitsinTOB_
-	<< "/" << minHitsinTEC_ << "/" << minHitsinBPIX_ << "/" << minHitsinFPIX_;
-    }
-  
+	  << "applying basic track cuts ..."
+	  << "\nptmin,ptmax:     " << ptMin_   << "," << ptMax_ 
+	  << "\netamin,etamax:   " << etaMin_  << "," << etaMax_
+	  << "\nphimin,phimax:   " << phiMin_  << "," << phiMax_
+	  << "\nnhitmin,nhitmax: " << nHitMin_ << "," << nHitMax_
+          << "\nnhitmin2D:       " << nHitMin2D_
+	  << "\nchi2nmax:        " << chi2nMax_;
+
   if (applyNHighestPt_)
 	edm::LogInfo("AlignmentTrackSelector") 
 	  << "filter N tracks with highest Pt N=" << nHighestPt_;
@@ -91,6 +74,21 @@ AlignmentTrackSelector::AlignmentTrackSelector(const edm::ParameterSet & cfg) :
 	edm::LogInfo("AlignmentTrackSelector") 
 	  << "apply multiplicity filter N>= " << minMultiplicity_ << "and N<= " << maxMultiplicity_
           << " on " << (multiplicityOnInput_ ? "input" : "output");
+
+  if (applyIsolation_)
+    edm::LogInfo("AlignmentTrackSelector") 
+      << "only retain tracks isolated at least by " << minHitIsolation_ << 
+      " cm from other rechits"; 
+
+  if (chargeCheck_)
+    edm::LogInfo("AlignmentTrackSelector") 
+      << "only retain hits with at least " << minHitChargeStrip_ <<  
+      " ADC counts of total cluster charge"; 
+
+  edm::LogInfo("AlignmentTrackSelector") 
+    << "Minimum number of hits in TIB/TID/TOB/TEC/BPIX/FPIX = " 
+    << minHitsinTIB_ << "/" << minHitsinTID_ << "/" << minHitsinTOB_
+    << "/" << minHitsinTEC_ << "/" << minHitsinBPIX_ << "/" << minHitsinFPIX_;
 
 }
 
@@ -128,15 +126,10 @@ AlignmentTrackSelector::select(const Tracks& tracks, const edm::Event& evt) cons
     }
   }
   
+  // edm::LogDebug("AlignmentTrackSelector") << "tracks all,kept: " << tracks.size() << "," << result.size();
+  
   return result;
 }
-
- ///returns if any of the Filters is used.
-bool AlignmentTrackSelector::useThisFilter()
-{
-  return applyMultiplicityFilter_  || applyBasicCuts_ || applyNHighestPt_ ;
-}
-
 
 // make basic cuts ------------------------------------------------------------
 
@@ -176,8 +169,7 @@ bool AlignmentTrackSelector::detailedHitsCheck(const reco::Track *trackp, const 
 
   if (minHitsinTIB_ || minHitsinTOB_ || minHitsinTID_ || minHitsinTEC_
       || minHitsinFPIX_ || minHitsinBPIX_ || nHitMin2D_ || chargeCheck_
-      || applyIsolation_ || (seedOnlyFromAbove_ == 1 || seedOnlyFromAbove_ == 2)) {
-    // any detailed hit cut is active, so have to check
+      || applyIsolation_) { // any detailed hit cut is active, so have to check
     
     int nhitinTIB = 0, nhitinTOB = 0, nhitinTID = 0;
     int nhitinTEC = 0, nhitinBPIX = 0, nhitinFPIX = 0;
@@ -185,24 +177,21 @@ bool AlignmentTrackSelector::detailedHitsCheck(const reco::Track *trackp, const 
     unsigned int thishit = 0;
 
     for (trackingRecHit_iterator iHit = trackp->recHitsBegin(); iHit != trackp->recHitsEnd(); ++iHit) {
+
       thishit++;
-      const DetId detId((*iHit)->geographicalId());
-      const int subdetId = detId.subdetId(); 
+      int type = (*iHit)->geographicalId().subdetId(); 
 
       // *** thishit == 1 means last hit in CTF *** 
       // (FIXME: assumption might change or not be valid for all tracking algorthms)
       // ==> for cosmics
       // seedOnlyFrom = 1 is TIB-TOB-TEC tracks only
       // seedOnlyFrom = 2 is TOB-TEC tracks only 
-      if (seedOnlyFromAbove_ == 1 && thishit == 1 
-          && (subdetId == int(StripSubdetector::TOB) || subdetId == int(StripSubdetector::TEC))){
-        return false; 
-      }
-      if (seedOnlyFromAbove_ == 2 && thishit == 1 && subdetId == int(StripSubdetector::TIB)) {
-        return false;
-      }
+      if (seedOnlyFromAbove_ == 1 && thishit == 1 && (type == int(StripSubdetector::TOB) || type == int(StripSubdetector::TEC))) return false; 
+
+      if (seedOnlyFromAbove_ == 2 && thishit == 1 && type == int(StripSubdetector::TIB)) return false;  
 
       if (!(*iHit)->isValid()) continue; // only real hits count as in trackp->numberOfValidHits()
+      const DetId detId((*iHit)->geographicalId());
       if (detId.det() != DetId::Tracker) {
         edm::LogError("DetectorMismatch") << "@SUB=AlignmentTrackSelector::detailedHitsCheck"
                                           << "DetId.det() != DetId::Tracker (=" << DetId::Tracker
@@ -211,12 +200,12 @@ bool AlignmentTrackSelector::detailedHitsCheck(const reco::Track *trackp, const 
       const TrackingRecHit* therechit = (*iHit).get();
       if (chargeCheck_ && !(this->isOkCharge(therechit))) return false;
       if (applyIsolation_ && (!this->isIsolated(therechit, evt))) return false;
-      if      (StripSubdetector::TIB == subdetId) ++nhitinTIB;
-      else if (StripSubdetector::TOB == subdetId) ++nhitinTOB;
-      else if (StripSubdetector::TID == subdetId) ++nhitinTID;
-      else if (StripSubdetector::TEC == subdetId) ++nhitinTEC;
-      else if (                kBPIX == subdetId) ++nhitinBPIX;
-      else if (                kFPIX == subdetId) ++nhitinFPIX;
+      if      (StripSubdetector::TIB == detId.subdetId()) ++nhitinTIB;
+      else if (StripSubdetector::TOB == detId.subdetId()) ++nhitinTOB;
+      else if (StripSubdetector::TID == detId.subdetId()) ++nhitinTID;
+      else if (StripSubdetector::TEC == detId.subdetId()) ++nhitinTEC;
+      else if (                kBPIX == detId.subdetId()) ++nhitinBPIX;
+      else if (                kFPIX == detId.subdetId()) ++nhitinFPIX;
       // Do not call isHit2D(..) if already enough 2D hits for performance reason:
       if (nHit2D < nHitMin2D_ && this->isHit2D(**iHit)) ++nHit2D;
     } // end loop on hits
@@ -262,62 +251,56 @@ bool AlignmentTrackSelector::isHit2D(const TrackingRecHit &hit) const
 
 //-----------------------------------------------------------------------------
 
-bool AlignmentTrackSelector::isOkCharge(const TrackingRecHit* hit) const
+bool AlignmentTrackSelector::isOkCharge(const TrackingRecHit* therechit) const
 {
-  if (!hit || !hit->isValid()) return true; 
-
-  // check det and subdet
-  const DetId id(hit->geographicalId());
-  if (id.det() != DetId::Tracker) {
-    edm::LogWarning("DetectorMismatch") << "@SUB=isOkCharge" << "Hit not in tracker!";
-    return true;
-  }
-  if (id.subdetId() == kFPIX || id.subdetId() == kBPIX) {
-    return true; // might add some requirement...
-  }
-
-  // We are in SiStrip now, so test normal hit:
-  const SiStripRecHit2D *stripHit2D = dynamic_cast<const SiStripRecHit2D*>(hit);
-  if (stripHit2D) {
-    return this->isOkChargeStripHit(stripHit2D);
-  }
-  // or matched (should not occur anymore due to hit splitting since 20X):
-  const SiStripMatchedRecHit2D *matchedHit = dynamic_cast<const SiStripMatchedRecHit2D*>(hit);
-  if (matchedHit) {  
-    return (this->isOkChargeStripHit(matchedHit->monoHit())
-            && this->isOkChargeStripHit(matchedHit->stereoHit()));
-  }
-  // or projected (should not occur anymore due to hit splitting since 20X):
-  const ProjectedSiStripRecHit2D *projHit = dynamic_cast<const ProjectedSiStripRecHit2D*>(hit);
-  if (projHit) {
-    return this->isOkChargeStripHit(&projHit->originalHit());
-  }
-
-  // and now? SiTrackerMultiRecHit? Not here I guess!
-  // Should we throw instead?
-  edm::LogError("AlignmentTrackSelector") 
-    << "@SUB=isOkCharge" << "Unknown valid tracker hit not in pixel, subdet " << id.subdetId()
-    << ", SiTrackerMultiRecHit " << dynamic_cast<const SiTrackerMultiRecHit*>(hit)
-    << ", BaseSiTrackerRecHit2DLocalPos " << dynamic_cast<const BaseSiTrackerRecHit2DLocalPos*>(hit); 
+  // const TrackingRecHit* therechit = hit.get();
+  float charge1 = 0;
+  float charge2 = 0;
+  const SiStripMatchedRecHit2D* matchedhit = dynamic_cast<const SiStripMatchedRecHit2D*>(therechit);
+  const SiStripRecHit2D* hit = dynamic_cast<const SiStripRecHit2D*>(therechit);
+  const ProjectedSiStripRecHit2D* unmatchedhit = dynamic_cast<const ProjectedSiStripRecHit2D*>(therechit);
   
+  if (matchedhit) {  
+    const SiStripRecHit2D *monohit=matchedhit->monoHit();    
+    const SiStripCluster* monocluster = &*(monohit->cluster());
+    const std::vector<uint16_t> amplitudesmono( monocluster->amplitudes().begin(),
+						monocluster->amplitudes().end());
+    for(size_t ia=0; ia<amplitudesmono.size();++ia)
+      { charge1+=amplitudesmono[ia];} 
+    
+    const SiStripRecHit2D *stereohit=matchedhit->stereoHit();   
+    const SiStripCluster* stereocluster = &*(stereohit->cluster());
+    const std::vector<uint16_t> amplitudesstereo( stereocluster->amplitudes().begin(),
+						  stereocluster->amplitudes().end());
+    for(size_t ia=0; ia<amplitudesstereo.size();++ia)
+      {charge2+=amplitudesstereo[ia];}
+    // std::cout << "charge1 = " << charge1 << "\n";
+    // std::cout << "charge2 = " << charge2 << "\n";
+    if (charge1 < minHitChargeStrip_ || charge2 < minHitChargeStrip_) return false;
+  }
+  else if (hit) {
+    
+    const SiStripCluster* cluster = &*(hit->cluster());
+    const std::vector<uint16_t> amplitudes( cluster->amplitudes().begin(),
+					    cluster->amplitudes().end());
+    for(size_t ia=0; ia<amplitudes.size();++ia)
+      {charge1+=amplitudes[ia];}
+    // std::cout << "charge1 = " << charge1 << "\n";
+    if (charge1 < minHitChargeStrip_) return false;
+  }
+  else if (unmatchedhit) {
+    
+    const SiStripRecHit2D &orighit = unmatchedhit->originalHit(); 
+    const SiStripCluster* origcluster = &*(orighit.cluster());
+    const std::vector<uint16_t> amplitudes( origcluster->amplitudes().begin(),
+					    origcluster->amplitudes().end());
+    for(size_t ia=0; ia<amplitudes.size();++ia)
+      {charge1+=amplitudes[ia];}
+    // std::cout << "charge1 = " << charge1 << "\n";
+    if (charge1 < minHitChargeStrip_) return false;
+  } 
   return true;
 } 
-
-//-----------------------------------------------------------------------------
-
-bool AlignmentTrackSelector::isOkChargeStripHit(const SiStripRecHit2D *siStripRecHit2D) const
-{
-  double charge = 0.;
-
-  SiStripRecHit2D::ClusterRef cluster(siStripRecHit2D->cluster());
-  const std::vector<uint16_t> &amplitudes = cluster->amplitudes();
-
-  for (size_t ia = 0; ia < amplitudes.size(); ++ia) {
-    charge += amplitudes[ia];
-  }
-
-  return (charge >= minHitChargeStrip_);
-}
 
 //-----------------------------------------------------------------------------
 
