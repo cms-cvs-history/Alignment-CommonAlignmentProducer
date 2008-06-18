4555297e606f1c40cd87838683c4cde7bf5fa0e2
cmsbuild <>
2008-06-18 22:19:34 +0000

    This commit was manufactured by cvs2git to create tag 'Alignment- CommonAlignmentProducer-V00-29-04'.
    Sprout from master 2008-06-18 22:19:33 UTC Jim Pivarski <sha1-4b35a9abbbfdbbd8cd887eda503efe9ec3a292e0@cern.ch> 'added ZeroFieldGlobalMuonBuilder (moved from HIPAlignmentAlgorithm)'
    Delete:
        data/ZeroFieldGlobalMuonBuilder.cfi
        plugins/ZeroFieldGlobalMuonBuilder.cc
        python/ZeroFieldGlobalMuonBuilder_cfi.py

--- python/ZeroFieldGlobalMuonBuilder_cfi.py
+++
@@ -1,9 +0,0 @@
-ZeroFieldGlobalMuonBuilder = cms.EDFilter("ZeroFieldGlobalMuonBuilder",
-                                          inputTracker = cms.InputTag("cosmictrackfinderP5"),
-                                          inputMuon = cms.InputTag("cosmicMuons"),
-                                          minTrackerHits = cms.int32(0),
-                                          minMuonHits = cms.int32(0),
-                                          minPdot = cms.double(0.99),
-                                          minDdotP = cms.double(0.99),
-                                          debuggingHistograms = cms.untracked.bool(False),
-                                          )
